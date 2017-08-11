c0d26a233230916e01a5ed9cf563bc304138510b
ManishBhakuni <patexample11@gmail.com>
2017-08-11 11:35:27 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,55 +1,6 @@
-module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-firefox-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('@angular/cli/plugins/karma'),
-      require('karma-coverage')
-    ],
-    client:{
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    files: [
-      { pattern: './src/test.ts', watched: false }
-    ],
-    preprocessors: {
-      'dist/app/**/!(*spec).js': ['coverage'],
-      './src/test.ts': ['@angular/cli']
-    },
-    mime: {
-      'text/x-typescript': ['ts','tsx']
-    },
-    coverageReporter: {
-      dir : 'coverage/',
-        reporters: [
-          { type: 'html' },
-          { type: 'lcov' }
-        ]
-    },
-    angularCli: {
-      config: './angular-cli.json',
-      codeCoverage: 'coverage',
-      environment: 'dev'
-    },
-    reporters: ['coverage', 'coveralls'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome', 'Firefox'],
-    singleRun: false
-  });
-};
-
-
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-/*
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -60,7 +11,7 @@ module.exports = function (config) {
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma'),
-	  require('karma-coverage')
+	    require('karma-coverage')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -77,11 +28,12 @@ module.exports = function (config) {
       fixWebpackSourcePaths: true
     },
     angularCli: {
+      config: './angular-cli.json',
       codeCoverage: 'coverage',
       environment: 'dev'
     },
-    reporters: ['progress', 'coverage', 'kjhtml'],
-	port: 9876,
+    reporters: ['coverage', 'coveralls'],
+	  port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
@@ -89,4 +41,4 @@ module.exports = function (config) {
     singleRun: false
   });
 };
-*/
+
