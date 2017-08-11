b45ace6b66d246a48be8d3c5b443afcea9122a81
ManishBhakuni <patexample11@gmail.com>
2017-08-11 11:09:10 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,56 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular/cli'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
+      require('@angular/cli/plugins/karma'),
+      require('karma-coverage')
+    ],
+    client:{
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    files: [
+      { pattern: './src/test.ts', watched: false }
+    ],
+    preprocessors: {
+      'dist/app/**/!(*spec).js': ['coverage'],
+      './src/test.ts': ['@angular/cli']
+    },
+    mime: {
+      'text/x-typescript': ['ts','tsx']
+    },
+    coverageReporter: {
+      dir : 'coverage/',
+        reporters: [
+          { type: 'html' },
+          { type: 'lcov' }
+        ]
+    },
+    angularCli: {
+      config: './angular-cli.json',
+      codeCoverage: 'coverage',
+      environment: 'dev'
+    },
+    reporters: config.angularCli && config.angularCli.codeCoverage
+              ? ['progress', 'coverage']
+              : ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome', 'Firefox'],
+    singleRun: false
+  });
+};
+
+
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+/*
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -40,3 +90,4 @@ module.exports = function (config) {
     singleRun: false
   });
 };
+*/
