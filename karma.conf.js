ac3eefdcb3508b38a1680ff0d0661bc530e9e94e
ManishBhakuni <patexample11@gmail.com>
2017-08-12 19:12:16 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -37,8 +37,7 @@ module.exports = function (config) {
 	  port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    #browsers: ['Chrome'],
+    autoWatch: true,    
     browsers: ['PhantomJS'],    
     singleRun: false
   });
